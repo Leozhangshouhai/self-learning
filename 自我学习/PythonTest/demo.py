63c5ceb0ceb14d33c7f8adec6b8d77cade06b598
a13551801507 <601216884@qq.com>
2017-07-14 15:36:17 +0800

    1

--- 自我学习/PythonTest/demo.py
+++ 自我学习/PythonTest/demo.py
@@ -1,14 +1,17 @@
-
+# -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest,time
-
-driver=webdriver.Chrome()
-driver.get("http://www.baidu.com")
-self.driver.maximize_window()
-builder = ActionChains(driver)
-builder.key_down(Keys.F12).perform()
-time.sleep(5)
+from time import sleep
+# ********************
+# 移动端测试专用模块
+WIDTH = 375
+HEIGHT = 667
+PIXEL_RATIO = 3.0
+UA = 'Mozilla/5.0 (Linux; Android 4.1.1; GT-N7100 Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/35.0.1916.138 Mobile Safari/537.36 T7/6.3'
+mobileEmulation = {"deviceMetrics": {"width": WIDTH, "height": HEIGHT, "pixelRatio": PIXEL_RATIO}, "userAgent": UA}
+options = webdriver.ChromeOptions()
+options.add_experimental_option('mobileEmulation', mobileEmulation)
+driver = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=options)
+driver.set_window_size(400,700)
+driver.get('http://m.baidu.com')
+#*********************
+sleep(3)
