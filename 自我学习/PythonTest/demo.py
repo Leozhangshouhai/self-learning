e11f467d8a8771c315df5da95b708673dbb3e67a
a13551801507 <601216884@qq.com>
2017-07-21 11:47:19 +0800

    no message

---
+++ 自我学习/PythonTest/demo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from time import sleep
+# ********************
+# 移动端测试专用模块
+WIDTH = 375
+HEIGHT = 667
+PIXEL_RATIO = 3.0
+UA = 'Mozilla/5.0 (Linux; Android 4.1.1; GT-N7100 Build/JRO03C) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/35.0.1916.138 Mobile Safari/537.36 T7/6.3'
+mobileEmulation = {"deviceMetrics": {"width": WIDTH, "height": HEIGHT, "pixelRatio": PIXEL_RATIO}, "userAgent": UA}
+options = webdriver.ChromeOptions()
+options.add_experimental_option('mobileEmulation', mobileEmulation)
+driver = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=options)
+driver.set_window_size(400,800)
+driver.get('http://www.zhuminsheng.com/ht/traffic/pages/trffic-violation.html')
+#*********************
+sleep(3)
